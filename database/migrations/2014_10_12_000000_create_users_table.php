6d1bc5da687cff38187f5e24ac08127908efb684
mbahgembrong <mbahgembrong.sos@gmail.com>
2023-02-21 03:19:23 +0700

    fix user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+              $table->string('nama');
+              $table->string('email')->unique();
+              $table->timestamp('email_verified_at')->nullable();
+              $table->string('password');
+              $table->string('jenis kelamin');
+             $table->text('alamat');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
