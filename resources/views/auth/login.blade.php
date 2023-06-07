c128ea1ec35e59fb38a21f8a6bdf345074abc57b
mbahgembrong.sos@gmail.com <mbah gembrong>
2023-06-07 15:51:53 +0700

    fix register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -82,8 +82,8 @@
                             <div class="card-body text-center">
                                 <div>
                                     <h2>Sign up</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna aliqua.</p>
+                                    <p>Belum punya akun, register dulu yuk !
+                                        Mari menjadi member di wijaya rental car.</p>
                                     <a class="btn btn-primary active mt-3"
                                         href="{{ url('/pelanggan/register') }}">Register
                                         Now!</a>
