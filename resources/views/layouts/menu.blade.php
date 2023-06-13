a18a3d2d4de83eb3ea92abf513d75c5157995203
mbahgembrong.sos@gmail.com <mbah gembrong>
2023-06-13 08:30:09 +0700

    fix menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -24,49 +24,49 @@
         </a>
     </li>
 @else
-    <li class="nav-item {{ Request::is('users*') ? 'active' : '' }}">
+    <li class="nav-item {{ Request::is('admin/home') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('home') }}">
             <i class="nav-icon fa fa-home" aria-hidden="true"></i>
             <span>Home</span>
         </a>
     </li>
-    <li class="nav-item {{ Request::is('users*') ? 'active' : '' }}">
+    <li class="nav-item {{ Request::is('admin/users*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('users.index') }}">
             <i class="nav-icon fa fa-user" aria-hidden="true"></i>
             <span>Data Admin</span>
         </a>
     </li>
-    <li class="nav-item {{ Request::is('pelanggans*') ? 'active' : '' }}">
+    <li class="nav-item {{ Request::is('admin/pelanggans*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('pelanggans.index') }}">
             <i class="nav-icon fa fa-users"></i>
             <span>Pelanggan</span>
         </a>
     </li>
-    <li class="nav-item {{ Request::is('sopirs*') ? 'active' : '' }}">
+    <li class="nav-item {{ Request::is('admin/sopirs*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('sopirs.index') }}">
             <i class="nav-icon fa fa-id-card"></i>
             <span>Data Sopir</span>
         </a>
     </li>
-    <li class="nav-item {{ Request::is('kategoriMobils*') ? 'active' : '' }}">
+    <li class="nav-item {{ Request::is('admin/kategoriMobils*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('kategoriMobils.index') }}">
             <i class="nav-icon fa fa-tags"></i>
             <span>Kategori Mobil</span>
         </a>
     </li>
-    <li class="nav-item {{ Request::is('mobils*') ? 'active' : '' }}">
+    <li class="nav-item {{ Request::is('admin/mobils*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('mobils.index') }}">
             <i class="nav-icon fa fa-car"></i>
             <span>Mobil</span>
         </a>
     </li>
-    <li class="nav-item {{ Request::is('rentals*') ? 'active' : '' }}">
+    <li class="nav-item {{ Request::is('admin/rentals*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('rentals.index') }}">
             <i class="nav-icon fa fa-taxi"></i>
             <span>Rental</span>
         </a>
     </li>
-    <li class="nav-item {{ Request::is('laporan*') ? 'active' : '' }}">
+    <li class="nav-item {{ Request::is('admin/laporan') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('laporan') }}">
             <i class="nav-icon fa fa-file-o"></i>
             <span>Laporan</span>
