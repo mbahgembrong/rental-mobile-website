beca1d0cd973003a0f8f7b17b573c6fd444ad741
levin <levinsilvia2510@gmail.com>
2023-06-21 11:06:32 +0700

    fix

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <ol class="breadcrumb">
-        <li class="breadcrumb-item">Pegawai</li>
+        <li class="breadcrumb-item">Admin</li>
     </ol>
     <div class="container-fluid">
         <div class="animated fadeIn">
@@ -12,7 +12,7 @@
                      <div class="card">
                          <div class="card-header">
                              <i class="fa fa-align-justify"></i>
-                             Pegawai
+                             Admin
                              <a class="pull-right" href="{{ route('users.create') }}"><i class="fa fa-plus-square fa-lg"></i></a>
                          </div>
                          <div class="card-body">
